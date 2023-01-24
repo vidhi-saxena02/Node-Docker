79879606a5f74db73aa09bcdc945b6a3c18fbf5e
vidhi-saxena02 <vidhisaxena02@yahoo.com>
2023-01-25 01:02:10 +0530

    mongoose added and mongo image added

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const PORT = 3000;
 const app = express();
+
+mongoose
+  .connect("mongodb://admin:password@34.238.155.118:27017/?authSource=admin")
+  .then(() => console.log("sucessfully connected"))
+  .catch((e) => console.log(e));
 
 app.get("/", (req, res) => {
   res.json({
