4311ce4b91b81dcefb9d012eb0745313c8920a3d
vidhi-saxena02 <vidhisaxena02@yahoo.com>
2023-01-27 01:01:18 +0530

    container bootup

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const {
+  MONGO_USER,
+  MONGO_PASSWORD,
+  MONGO_IP,
+  MONGO_PORT,
+} = require("./config/config");
 const PORT = 3000;
 const app = express();
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 
-mongoose
-  .connect("mongodb://admin:password@mongo:27017/?authSource=admin")
-  .then(() => console.log("sucessfully connected"))
-  .catch((e) => console.log(e));
+const connectWithRetry = () => {
+  mongoose
+    .connect(mongoURL)
+    .then(() => console.log("sucessfully connected"))
+    .catch((e) => {
+      console.log(e);
+      setTimeout(connectWithRetry, 5000);
+    });
+};
+
+connectWithRetry();
 
 app.get("/", (req, res) => {
   res.json({
